890d0bccd846f62f874b7960ed0f741d818369dc
Quema <myoon0929@gmail.com>
2023-06-09 17:38:41 +0900

    add

--- point.js
+++ point.js
@@ -1,8 +1,13 @@
 function click (){
     // 특정 요소의 생성을 감지하여 자동 클릭
-    const targetElement = document.querySelector('button[aria-label="Claim Bonus"]');
-    if (targetElement) {
-        targetElement.click();
+    const targetElementen = document.querySelector('button[aria-label="Claim Bonus"]');
+    if (targetElementen) {
+        targetElementen.click();
+    }
+        // 특정 요소의 생성을 감지하여 자동 클릭
+    const targetElementko = document.querySelector('button[aria-label="보너스 받기"]');
+    if (targetElementko) {
+        targetElementko.click();
     }
 }
 
