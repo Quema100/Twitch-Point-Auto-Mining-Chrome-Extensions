0f5345e8fe00f2e155a84e1b3e0947cc63eebd97
Quema <myoon0929@gmail.com>
2023-06-12 20:49:11 +0900

    fix

--- console.js
+++ console.js
@@ -1,15 +1,15 @@
 function tab() {
-    if (chrome.tabs) {
-      chrome.tabs.query({ url: "https://www.twitch.tv/*" }, function(tabs) {
-        tabs.forEach(function (tabs2) { () => {
-            chrome.tabs.executeScript(tabs2.id, { code: `(${click})();` });
-        }
-       });
-       })
-    } else {
-      console.error("chrome.tabs API가 로드되지 않았습니다.");
-    }
+  if (chrome.tabs) {
+    chrome.tabs.query({ url: "https://www.twitch.tv/*" }, function(tabs) {
+      tabs.forEach(function(tab) {
+        chrome.tabs.sendMessage(tab.id, { action: "click" });
+      });
+    });
+  } else {
+    console.error("chrome.tabs API가 로드되지 않았습니다.");
+  }
 }
-setInterval(()=>{
-    tab()
-},1000)
+
+setInterval(() => {
+  tab();
+}, 1000);
--- point.js
+++ point.js
@@ -1,16 +1,22 @@
-function click (){
-    // 특정 요소의 생성을 감지하여 자동 클릭
-    const targetElementen = document.querySelector('button[aria-label="Claim Bonus"]');
-    if (targetElementen) {
-        targetElementen.click();
-    }
-        // 특정 요소의 생성을 감지하여 자동 클릭
-    const targetElementko = document.querySelector('button[aria-label="보너스 받기"]');
-    if (targetElementko) {
-        targetElementko.click();
-    }
+function click() {
+  // 특정 요소의 생성을 감지하여 자동 클릭
+  const targetElementen = document.querySelector('button[aria-label="Claim Bonus"]');
+  if (targetElementen) {
+    targetElementen.click();
+  }
+
+  // 특정 요소의 생성을 감지하여 자동 클릭
+  const targetElementko = document.querySelector('button[aria-label="보너스 받기"]');
+  if (targetElementko) {
+    targetElementko.click();
+  }
 }
 
-setInterval(()=>{
-    click ()
-},1000)
+chrome.runtime.sendMessage({ action: "click" });
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "click") {
+    click();
+    console.log('작동중')
+  }
+});
