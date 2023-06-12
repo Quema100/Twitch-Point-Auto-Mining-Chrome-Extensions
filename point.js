21601893567daf83909c744edcea19627c3c2425
Quema <myoon0929@gmail.com>
2023-06-12 21:03:28 +0900

    fix

--- point.js
+++ point.js
@@ -1,22 +1,21 @@
-function click() {
-  // 특정 요소의 생성을 감지하여 자동 클릭
-  const targetElementen = document.querySelector('button[aria-label="Claim Bonus"]');
-  if (targetElementen) {
-    targetElementen.click();
-  }
-
-  // 특정 요소의 생성을 감지하여 자동 클릭
-  const targetElementko = document.querySelector('button[aria-label="보너스 받기"]');
-  if (targetElementko) {
-    targetElementko.click();
-  }
-}
-
-chrome.runtime.sendMessage({ action: "click" });
-
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "click") {
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === "performClick") {
     click();
-    console.log('작동중')
   }
 });
+
+function click() {
+  // 특정 요소의 생성을 감지하여 자동 클릭
+  const targetElementEn = document.querySelector('button[aria-label="Claim Bonus"]');
+  if (targetElementEn) {
+    targetElementEn.click();
+    console.log('포인트 얻기 완료')
+  }
+
+  // 특정 요소의 생성을 감지하여 자동 클릭
+  const targetElementKo = document.querySelector('button[aria-label="보너스 받기"]');
+  if (targetElementKo) {
+    targetElementKo.click();
+    console.log('포인트 얻기 완료')
+  }
+}
