f404b585fa8b35d6e2a36cc16e3663728d61414a
Quema <myoon0929@gmail.com>
2023-06-14 00:24:04 +0900

    fix

--- console.js
+++ console.js
@@ -1,25 +1,56 @@
-function nowtab() {
-  if (chrome.tabs) {
-    chrome.tabs.query({ url: "https://www.twitch.tv/*" }, function(tabs) {
-      for (let i = 0; i < tabs.length; i++) {
-        chrome.tabs.update(tabs[i].id, { active: true }, function(tab) {
-          if (chrome.runtime.lastError) {
-            // Error occurred, retry after a short delay
-            console.error(chrome.runtime.lastError.message);
-            setTimeout(function() {
-              nowtab(); // Retry the tab activation
-            }, 1000); // Delay in milliseconds (adjust as needed)
-          } else {
-            console.log("Tab activated:", tab);
-          }
-        });
+function activateAllTabs() {
+  chrome.tabs.query({ url: "https://www.twitch.tv/*" }, function(tabs) {
+    let index = 0;
+
+    function activateNextTab() {
+      if (index < tabs.length) {
+        const tab = tabs[index];
+        if (!tab.active && !tab.discarded) {
+          chrome.tabs.get(tab.id, function(existingTab) {
+            if (chrome.runtime.lastError || !existingTab) {
+              activateAllTabs()
+            } else {
+              chrome.tabs.update(existingTab.id, { active: true }, function(updatedTab) {
+                if (chrome.runtime.lastError) {
+                  console.error(chrome.runtime.lastError.message);
+                } else {
+                  console.log("Tab activated:", updatedTab);
+                }
+              });
+            }
+          });
+        }
+        index++; // Move to the next tab
       }
-    });
-  } else {
-    console.error("chrome.tabs API가 로드되지 않았습니다.");
-  }
+
+      if (index >= tabs.length) {
+        // All tabs have been processed
+        index = 0; // Reset index for the next iteration
+      }
+
+      setTimeout(activateNextTab, 4000); // Delay before activating the next tab
+    }
+
+    activateNextTab();
+  });
 }
 
-setInterval(() => {
-  nowtab();
-}, 1000);
+activateAllTabs(); // 최초 접속 시 작동
+
+// 탭의 상태 변경을 감지하여 처리
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status === "complete" && tab.url.startsWith("https://www.twitch.tv/")) {
+    activateAllTabs();
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  chrome.tabs.query({ url: "https://www.twitch.tv/*" }, function(tabs) {
+    // Check if the removed tab is included in the current tabs list
+    const removedTab = tabs.find(tab => tab.id === tabId);
+    if (!removedTab) {
+      // Tab is no longer present, adjust the loop
+      activateAllTabs();
+    }
+  });
+});
